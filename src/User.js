076a90ac00ab842c698395d331a3750d088c4e7f
anabelaSantos <anabelamsantos@gmail.com>
2023-02-10 20:08:44 +0000

    user and user test

--- src/User.js
+++ src/User.js
@@ -1,5 +1,21 @@
 class User {
   // User code here
+
+  constructor({ username, password, age }) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false; //the loggedIn is a property and is just initialized to false
+  }
+
+  //the methods are assigned to properties in the object because they're in a constructor, not an object literal
+
+  login = (password) => {
+    this.loggedIn = true;
+  };
+  logout = () => {
+    this.loggedIn = false;
+  };
 }
 
-module.exports = User
+module.exports = User;
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,12 +1,32 @@
-const User = require('../src/User')
+const User = require("../src/User");
 
 // User tests here
-
-// test username
-
-// test password
-
-// test age
+describe("User class integrity checks", () => {
+  test("can create new user instance", () => {
+    let user1 = new User({
+      username: "Anabela",
+      password: "thisIsIt",
+      age: "75",
+      loggedIn: true,
+    });
+    expect(typeof user1).toBe("object");
+    expect(user1).toHaveProperty("username", "Anabela"); // test username
+    expect(user1).toHaveProperty("password", "thisIsIt");
+    expect(user1).toHaveProperty("age", "75"); // test age
+  });
+  //test loogedIn status
+  test("Have LoggedIn status", () => {
+    let user2 = new User({
+      username: "Prince",
+      password: "ofPersia",
+      age: "15",
+      loggedIn: false,
+    });
+    expect(typeof user2.loggedIn).toBe("boolean");
+    expect(user2.loggedIn).toBe(false);
+  });
+  test("");
+});
 
 // test login
 
