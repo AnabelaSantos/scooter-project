af8feb7d4462da8081b2e9a4d18ac727def3c497
anabelaSantos <anabelamsantos@gmail.com>
2023-02-12 18:30:26 +0000

    scooter and scooter tests

--- src/Scooter.js
+++ src/Scooter.js
@@ -11,6 +11,42 @@ class Scooter {
     this.charge = 100;
     this.isBroken = false;
   }
+
+  // Scooter method rent
+
+  rent() {
+    if (this.charge > 20 && this.isBroken === false) {
+      this.station = null;
+    }
+    if (this.charge <= 20) {
+      throw new Error("scooter needs to charge");
+    }
+    if (this.isBroken) {
+      throw new Error("scooter needs repair");
+    }
+  }
+  // Scooter method dock
+  dock(station) {
+    this.station = station;
+  }
+
+  // Scooter method recharge
+  async charge() {
+    console.log("Starting charge");
+
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // wait 2 seconds
+    this.charge = 100;
+
+    console.log("Charge complete");
+  }
+
+  //Scooter method request repair
+  async isBroken() {
+    await new Promise((resolve) => setTimeout(resolve, 5000)); // wait 5 seconds
+    this.isBroken = false;
+
+    console.log("repair completed");
+  }
 }
 
 module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -24,13 +24,6 @@ describe("scooter object", () => {
     expect(scooter1.isBroken).toBe(false);
   });
   test("test next serial number", () => {
-    let user1 = new User({
-      username: "Anabela",
-      password: "thisIsIt",
-      age: 75,
-      loggedIn: true,
-    });
-
     let scooter2 = new Scooter({
       station: "Vauxhall Bridge",
     });
@@ -42,7 +35,72 @@ describe("scooter object", () => {
 describe("scooter methods", () => {
   // tests here!
   //rent method
+  test("if the scooter is charged above 20 and it's not broken", () => {
+    let scooter3 = new Scooter({
+      station: "London Eye",
+    });
+    scooter3.charge = 25;
+    expect(scooter3.charge).toBeGreaterThan(20);
+    scooter3.isBroken = false;
+    expect(scooter3.isBroken).toBe(false);
+  });
+
+  test("check it out to user", () => {
+    let user1 = new User({
+      username: "Anabela",
+      password: "thisIsIt",
+      age: 75,
+      loggedIn: true,
+    });
+
+    let scooter4 = new Scooter({
+      station: "British Museum",
+    });
+    scooter4.charge = 50;
+    scooter4.isBroken = false;
+    scooter4.user = user1;
+    expect(scooter4.user).toBe(user1);
+  });
+  // test error messages
+  test("scooter needs to charge", () => {
+    let scooter5 = new Scooter({
+      station: "Station5",
+    });
+    scooter5.charge = 10;
+    scooter5.isBroken = false;
+    function needsCharge() {
+      scooter5.rent();
+    }
+    expect(needsCharge).toThrow("scooter needs to charge");
+  });
+  test("scooter needs repair", () => {
+    let scooter6 = new Scooter({
+      station: "Station6",
+    });
+    scooter6.charge = 50;
+    scooter6.isBroken = true;
+    function needsRepair() {
+      scooter6.rent();
+    }
+    expect(needsRepair).toThrow("scooter needs repair");
+  });
   //dock method
-  //requestRepair method
+  test("dock scooter to station", () => {
+    let scooter = new Scooter("station7");
+    scooter.rent();
+    scooter.dock("station8");
+    expect(scooter).toHaveProperty("station", "station8");
+  });
   //charge method
+  test("charge", async () => {
+    const scooter = new Scooter("station9");
+    await scooter.charge; // we need to wait for the charge!
+    expect(scooter.charge).toBe(100);
+  });
+  //requestRepair method
+  test("repair", async () => {
+    const scooter = new Scooter("station9");
+    await scooter.isBroken; // we need to wait for the charge!
+    expect(scooter.isBroken).toBe(false);
+  });
 });
