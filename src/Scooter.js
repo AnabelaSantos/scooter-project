65989cc32a2c98239dc6758dbe50dcc867d34fe9
anabelaSantos <anabelamsantos@gmail.com>
2023-02-12 15:36:32 +0000

    scooter and scooter tests parameters

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,16 @@
-class Scooter{
+class Scooter {
+  // Stactic initialization block
+  static nextSerial = 1;
+
   // scooter code here
+  constructor({ station }) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial;
+    Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,48 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+describe("scooter object", () => {
+  test("can create new scooter instance", () => {
+    let user1 = new User({
+      username: "Anabela",
+      password: "thisIsIt",
+      age: 75,
+      loggedIn: true,
+    });
+
+    let scooter1 = new Scooter({
+      station: "London Bridge",
+    });
+
+    expect(typeof scooter1).toBe("object");
+    expect(scooter1).toHaveProperty("station", "London Bridge"); // test station
+    expect(scooter1).toHaveProperty("user", null); //test user
+    expect(scooter1).toHaveProperty("serial", 1); // test serial
+    expect(scooter1).toHaveProperty("charge", 100); // test charge
+    expect(typeof scooter1.isBroken).toBe("boolean");
+    expect(scooter1.isBroken).toBe(false);
+  });
+  test("test next serial number", () => {
+    let user1 = new User({
+      username: "Anabela",
+      password: "thisIsIt",
+      age: 75,
+      loggedIn: true,
+    });
+
+    let scooter2 = new Scooter({
+      station: "Vauxhall Bridge",
+    });
+    expect(scooter2).toHaveProperty("serial", 2); // test next serial
+  });
+});
 
 //Method tests
-describe('scooter methods', () => {
+describe("scooter methods", () => {
   // tests here!
-
   //rent method
-
   //dock method
-
   //requestRepair method
-
   //charge method
-
-})
+});
