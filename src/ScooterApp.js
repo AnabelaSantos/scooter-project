3601c7357c50af2186ff36d7267de0a8a1bdff38
anabelaSantos <anabelamsantos@gmail.com>
2023-02-12 23:55:43 +0000

    scooter app and scooter app missing dock and print methods

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,8 +1,89 @@
-const User = require('./User')
-const Scooter = require('./Scooter')
+const User = require("./User");
+const Scooter = require("./Scooter");
 
 class ScooterApp {
   // ScooterApp code here
+  constructor() {
+    this.stations = {
+      station1: [],
+      station2: [],
+      station3: [],
+    };
+
+    this.registeredUsers = {};
+  }
+  //Register User method
+  registerUser(username, password, age) {
+    if (username in this.registeredUsers) {
+      throw new Error("already registered");
+    }
+    if (age < 18) {
+      throw new Error("too young to register");
+    }
+    let user = new User({
+      username: username,
+      password: password,
+      age: age,
+    });
+    this.registeredUsers[username] = user;
+    console.log("user has been registered");
+    return user;
+  }
+
+  //loginUser method
+
+  loginUser(username, password) {
+    if (
+      !username in this.registeredUsers ||
+      password !== this.registeredUsers[username].password
+    ) {
+      throw new Error("Username or password is incorrect");
+    }
+    this.registeredUsers[username].login(password);
+    console.log("user has been logged in");
+  }
+
+  //logoutUser method
+
+  logoutUser(username) {
+    if (!(username in this.registeredUsers)) {
+      throw new Error("no such user is logged in");
+    }
+    this.registeredUsers[username].logout();
+    console.log("user is logged out");
+  }
+
+  //Create Scooter method
+  createScooter(station) {
+    if (!station in this.stations) {
+      throw new Error("no such station");
+    }
+
+    let scooter = new Scooter({
+      station: station,
+    });
+    this.stations[station].push(scooter);
+    console.log("created new scooter");
+    return scooter;
+  }
+
+  //Rent Scooter method
+  rentScooter(scooter, user) {
+    if (scooter.user !== null) {
+      throw new Error("scooter already rented");
+    }
+    let station = scooter.station;
+    for (let i = 0; i < this.stations[station].length; i++) {
+      let currentScooter = this.stations[station][i];
+      if (scooter.serial === currentScooter.serial) {
+        this.stations[station].splice(i, 1);
+      }
+    }
+    scooter.user = user;
+
+    console.log("scooter is rented");
+  }
+  // Dock Scooter method
 }
 
-module.exports = ScooterApp
+module.exports = ScooterApp;
--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -3,13 +3,159 @@ const User = require("../src/User");
 const ScooterApp = require("../src/ScooterApp");
 
 // ScooterApp tests here
+describe("scooterApp object", () => {
+  test("can create new scooterApp instance", () => {
+    let scooterApp1 = new ScooterApp();
 
+    expect(typeof scooterApp1).toBe("object");
+    expect(scooterApp1).toHaveProperty("stations", {
+      station1: [],
+      station2: [],
+      station3: [],
+    });
+    expect(scooterApp1).toHaveProperty("registeredUsers", {});
+  });
+});
 // register user
+describe("registerUser method", () => {
+  test("if the user is not already registered", () => {
+    let scooterApp = new ScooterApp();
+    let user1 = scooterApp.registerUser("Anabela", "thisIsIt", 75);
+    expect(typeof user1).toBe("object");
+    expect(user1).toHaveProperty("username", "Anabela"); // test username
+    expect(user1).toHaveProperty("password", "thisIsIt");
+    expect(user1).toHaveProperty("age", 75); // test age
+    expect(scooterApp.registeredUsers).toHaveProperty("Anabela");
+  });
+  test("User has been registered Error", () => {
+    let scooterApp = new ScooterApp();
+    scooterApp.registerUser("Anabela", "thisIsIt", 75);
 
+    function hasBeenRegistered() {
+      scooterApp.registerUser("Anabela", "thisIsIt", 75);
+    }
+    expect(hasBeenRegistered).toThrow("already registered");
+  });
+  test("Too young to register Error", () => {
+    let scooterApp = new ScooterApp();
+    scooterApp.registerUser("Anabela", "thisIsIt", 75);
+
+    function isTooYoung() {
+      scooterApp.registerUser("Joao", "thisPass", 15);
+    }
+    expect(isTooYoung).toThrow("too young to register");
+  });
+});
 // log in
+describe("loginUser method", () => {
+  test("if the user is located by name and test correct login", () => {
+    let scooterApp = new ScooterApp();
+    let user = scooterApp.registerUser("Anabela", "thisIsIt", 75);
+    scooterApp.loginUser("Anabela", "thisIsIt");
+    expect(user).toHaveProperty("loggedIn", true);
+  });
+  test("Username or password incorrect Error", () => {
+    let scooterApp = new ScooterApp();
+    scooterApp.registerUser("Anabela", "thisIsIt", 75);
 
-// log out
+    function userOrPassWrong() {
+      scooterApp.loginUser("Anabela", "wrongPass");
+    }
+    expect(userOrPassWrong).toThrow("Username or password is incorrect");
+  });
 
-// rent scooter
+  // log out
+  describe("logoutUser method", () => {
+    test("if the user is located by name and test correct logout", () => {
+      let scooterApp = new ScooterApp();
+      let user = scooterApp.registerUser("Anabela", "thisIsIt", 75);
+      scooterApp.logoutUser("Anabela");
+      expect(user).toHaveProperty("loggedIn", false);
+    });
+    test("No such User is Logged In Error", () => {
+      let scooterApp = new ScooterApp();
+      scooterApp.registerUser("Anabela", "thisIsIt", 75);
 
-// dock scooter
+      function notLoggedIn() {
+        scooterApp.logoutUser("joao");
+      }
+      expect(notLoggedIn).toThrow("no such user is logged in");
+    });
+  });
+
+  //create  Scooter method
+  describe("createScooter method", () => {
+    test("new scooter is created and added to station", () => {
+      let scooterApp = new ScooterApp();
+      let scooter = scooterApp.createScooter("station1");
+      expect(typeof scooter).toBe("object");
+
+      expect(scooter).toHaveProperty("station", "station1");
+      expect(scooterApp.stations).toHaveProperty("station1", [scooter]);
+    });
+    test("No such Station Error", () => {
+      let scooterApp = new ScooterApp();
+
+      function noStation() {
+        let scooter = scooterApp.createScooter("Good Street");
+        expect(noStation).toThrow("no such station");
+      }
+    });
+  });
+  // rent scooter
+
+  describe("Rent Scooter method", () => {
+    test("Scooter is located and removed from station, rented to the user", () => {
+      let scooterApp = new ScooterApp();
+      let scooter = scooterApp.createScooter("station1");
+
+      let user2 = new User({
+        username: "Prince",
+        password: "ofPersia",
+        age: 57,
+        loggedIn: true,
+      });
+      scooterApp.rentScooter(scooter, user2);
+
+      expect(scooterApp.stations).toHaveProperty("station1", []);
+      expect(scooter.user).toMatchObject(user2);
+    });
+    test("Scooter Already rented Error", () => {
+      let scooterApp = new ScooterApp();
+      let scooter = scooterApp.createScooter("station1");
+      let user1 = new User({
+        username: "Prince",
+        password: "ofPersia",
+        age: 57,
+        loggedIn: true,
+      });
+      scooterApp.rentScooter(scooter, user1);
+      function alreadyRented() {
+        let user2 = new User({
+          username: "Bob",
+          password: "theBuilder",
+          age: 79,
+        });
+        scooterApp.rentScooter(scooter, user2);
+        expect(alreadyRented).toThrow("scooter already rented");
+      }
+    });
+  });
+
+  //   // dock scooter method
+  //   describe("dock scooter method", () => {
+  //     test("new scooter is docked added to station's scooter list", () => {
+  //       let scooterApp = new ScooterApp();
+  //       let scooter = scooterApp.createScooter("station1");
+
+  //     });
+  //     test("No such Station Error", () => {
+  //       let scooterApp = new ScooterApp();
+
+  //       function noStation() {
+  //         let scooter = scooterApp.createScooter("Good Street");
+  //         expect(noStation).toThrow("no such station");
+  //       }
+  //     });
+  //   });
+});
