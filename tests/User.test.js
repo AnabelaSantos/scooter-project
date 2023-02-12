c99b4fd620dedb3167edbb06917b9dc62d9fcb06
anabelaSantos <anabelamsantos@gmail.com>
2023-02-12 12:44:28 +0000

    user age as a number

--- tests/User.test.js
+++ tests/User.test.js
@@ -6,13 +6,13 @@ describe("User class integrity checks", () => {
     let user1 = new User({
       username: "Anabela",
       password: "thisIsIt",
-      age: "75",
+      age: 75,
       loggedIn: true,
     });
     expect(typeof user1).toBe("object");
     expect(user1).toHaveProperty("username", "Anabela"); // test username
     expect(user1).toHaveProperty("password", "thisIsIt");
-    expect(user1).toHaveProperty("age", "75"); // test age
+    expect(user1).toHaveProperty("age", 75); // test age
   });
 });
 //test loogedIn status
@@ -21,7 +21,7 @@ describe("User class looged status", () => {
     let user2 = new User({
       username: "Prince",
       password: "ofPersia",
-      age: "15",
+      age: 15,
       loggedIn: false,
     });
     expect(typeof user2.loggedIn).toBe("boolean");
